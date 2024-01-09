83bf6b97f7c037ca9f67650f64eea4426e30d27c
Kushagra-23 <kshgrkumar@gmail.com>
2024-01-09 23:12:45 +0530

    recursive fibonaaci sequence done

---
+++ recursiveFibonacciSequence.js
@@ -0,0 +1,14 @@
+// Problem : Give a number 'n', find the nth element of the fibonacci sequence
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+// Big-O - O(2^n)
+
+console.log("Result: ", recursiveFibonacci(0));
+console.log("Result: ", recursiveFibonacci(1));
+console.log("Result: ", recursiveFibonacci(6));
